c441a58f30d5ce6f357e2faf0d2bc2dbdbbf6ee7
Aristotelis Liakatas <agliak@gmail.com>
2019-10-06 18:08:48 +0100

    Kalman class equipped with necessary methods to initialise, train itself and provide corrections to input values. A method to show current values of the filter is also provided. The script for testing uses a basic dataset created each time it is run.

--- Kalman/Kalman.py
+++ Kalman/Kalman.py
@@ -20,206 +20,237 @@ class Kalman:
         self.dim = dim
         self.sq_shape = (dim, dim)
         self.vec_shape = (dim, 1)
+        self.nTrained = 0
 
-        # Get the transition matrix
-        if F is None:
-            self.F = np.eye(self.dim)
-        else:
-            if F.shape[0] == self.dim:
-                self.F = np.array(F)
-            else:
-                raise ValueError('Transition (system) matrix F is has the wrong dimensions.')
-        
-        # Get the covariance matrix
-        if P is None:
-            self.P = 10.0 * np.ones(self.sq_shape)
-        else:
-            if isinstance(P, list):
-                if P.shape[0] == self.dim:
-                    self.P = np.array(P)
-                else:
-                    raise ValueError('Covariance matrix P has the wrong dimensions.')
-            else:
-                self.P = P * np.ones(self.sq_shape)
+        # Set the system matrix based on entries
+        self.set_system_matrix(F)
+
+        # Set the covariance matrix based on entries
+        self.set_covariance_matrix(P)
 
         # Hope it's classic Kalman, but you never know
         self.classic = True
+        self.exp_var = None
         if not (exp_var is None):
             print('Switching to Information Geometry Kalman filter...')
             print('Variance for data is provided explicitly as: {}'.format(exp_var))
             self.classic = False
-            self.variance = exp_var
-        else:
-            self.variance = 0.0 
-
-        self.covariance = 0.0 #self.calculate_covariance()
+            self.exp_var = exp_var
+        # Most likely, it's not going to be used right away, but planning ahead
 
         # Initialise other relevant matrices
         self.X = np.zeros(self.vec_shape)    # State vector
         self.H = np.zeros(self.vec_shape)    # Observations matrix
         self.KG = np.zeros(self.vec_shape)   # Kalman gain 
-        self.Q = np.eye(self.dim)            # Variance matrix
-        self.R = 6                           # Covariance 
 
         # Create variables to keep history data
-        self.x_history = np.zeros((self.dim, self.history))
-        self.y_history = np.zeros((self.history, 1))
+        self.x_history = np.zeros((self.dim, self.history + 1))
+        self.H_history = np.zeros((self.dim, self.history + 1))
+        self.y_history = np.zeros((self.history + 1, 1))
 
-    def dump_members(self, ij = None):
+        
+    def set_system_matrix(self, ff):
+        # Get the system matrix
+        if ff is None:
+            self.F = np.eye(self.dim)
+        else:
+            if ff.shape[0] == self.dim:
+                self.F = np.array(ff)
+            else:
+                raise ValueError('Transition (system) matrix F has the wrong dimensions.')
+        return
+
+    def set_covariance_matrix(self, pp):
+        # Get the covariance matrix
+        if pp is None:
+            self.P = 10.0 * np.ones(self.sq_shape)
+        else:
+            if isinstance(pp, list):
+                if pp.shape[0] == self.dim:
+                    self.P = np.array(pp)
+                else:
+                    raise ValueError('Covariance matrix P has the wrong dimensions.')
+            else:
+                self.P = pp * np.ones(self.sq_shape)
+        return 
+    
+    def set_observations_matrix(self, val):
+        # Get the observations matrix from 
+        # current model's prediction
+        for ij in range(self.dim):
+            self.H[ij, 0] = val ** ij
+        return
+        
+    def print_2D_mat(self, mat, txt):
+        # Just show me the contents
+        print('{}:\t \t{}'.format(txt, mat[0, :]))
+        for ij in range(1,self.dim):
+            print(' \t \t{}'.format(mat[ij, :]))
+        return
+
+    def print_1D_mat(self, mat, txt):
+        # Just show me the contents
+        print('{}:\t \t{}'.format(txt, mat[:, 0]))
+
+    def update_x_history(self, xx):
+        # Update the x-history
+        self.x_history[:, 0:-1] = self.x_history[:, 1:]
+        self.x_history[:, -1] = xx[:, 0]
+        return
+
+    def update_H_history(self, hh):
+        # Update the H-history 
+        self.H_history[:, 0:-1] = self.H_history[:, 1:]
+        self.H_history[:, -1] = hh[:, 0]
+        return
+
+    def update_y_history(self, yy):
+        # Update the y-history
+        self.y_history[0:-1, 0] = self.y_history[1:, 0]
+        self.y_history[-1, 0] = yy
+        return
+
+    def get_state_variance(self):
+        # Calculate the variance 
+        # from the x-history
+        sx = np.zeros_like(self.X)
+        for ij in range(self.history):
+            sx[:, 0] += self.x_history[:, ij] - self.x_history[:, ij + 1]
+        sx /= self.history
+
+        Wt = np.zeros_like(self.P)
+        for ij in range(self.history):
+            temp = self.x_history[:, ij] - self.x_history[:, ij + 1] - sx 
+            Wt += np.dot(temp, temp.T)
+            
+        Wt /= (self.history - 1.0)
+        return Wt
+
+    def get_observation_variance(self):
+        # Calculate the variance
+        # from the y-history
+        sy = 0.0
+        for ij in range(self.history):
+            sy += self.y_history[ij, 0] - np.dot(self.H_history[:, ij].T, self.x_history[:, ij])
+        sy /= self.history
+
+        Vt = 0.0
+        for ij in range(self.history):
+            temp = self.y_history[ij, 0] - np.dot(self.H_history[:, ij].T, self.x_history[:, ij]) - sy
+            Vt += temp ** 2
+
+        Vt /= (self.history - 1.0)
+        return Vt
+        
+    def predict(self):
+        # Provide an optimal estimate
+        # for State vector and Covariance
+        # matrix
+        W = self.get_state_variance()
+        self.X_est = np.dot(self.F, self.X)
+        self.P_est = np.dot(np.dot(self.F, self.P), self.F.T) + W
+        return
+
+    def update(self, y):
+        # Provide an update of 
+        # the filter's state
+        # based on new input
+        
+        # Get the variance of the dataset
+        if self.classic:
+            vt = self.get_observation_variance()
+        else:
+            vt = self.exp_var
+
+        # Calculate the value for denominator
+        denom = np.dot(np.dot(self.H.T, self.P_est), self.H) + vt
+        
+        # Kalman Gain
+        self.KG = np.dot(self.P_est, self.H) / denom
+
+        # Get new state
+        self.X = self.X + self.KG * (y - np.dot(self.H.T, self.X_est))
+
+        # Get new covariance matrix
+        self.P = np.dot(np.eye(self.dim) - np.dot(self.KG, self.H.T), self.P_est)
+
+        # Update history data
+        self.update_x_history(self.X)
+        self.update_H_history(self.H)
+        self.update_y_history(y)
+        return 
+
+    def train_me(self, obs, model, showProrgess = False):
+        """
+        Master method to control the initial 
+        training of the filter.
+        """
+        # Ensure they are numpy arrays
+        myobs = np.array(obs)
+        mymodel = np.array(model)
+        
+        # Check if the dimensions match
+        if myobs.shape != mymodel.shape:
+            raise TypeError('Initial training set does not have conforming shapes.')
+        
+        NN = len(myobs)
+
+        # Train it using all the available data
+        for ih in range(NN):
+            
+            self.nTrained += 1
+            if showProrgess:
+                print('Training #{}'.format(self.nTrained))
+
+            y = myobs[ih] - mymodel[ih]
+
+            self.set_observations_matrix(mymodel[ih])
+
+            self.predict()
+
+            self.update(y)
+
+            if showProrgess:
+                self.dump_members()
+                print('************************************ \n')
+        return
+
+    def adjust_forecast(self, val, buff = 20.0):
+        """
+        Method to provide an adjustment to 
+        the forecast value based on current
+        state of the filter.
+        """
+        prod = np.dot(self.H.T, self.X)
+        ret = val + prod
+
+        if ret <= 0.0:
+            ret = 0.0
+        elif abs(prod) >= buff:
+            ret = val
+        return ret
+
+    def dump_members(self):
         """
         Defining the "print" method for 
         debugging and informative purposes.
         """
         print('--------------------------')
         print('     Kalman Instance      ')
-        if not (ij is None):
-            print('({})'.format(ij))
-        print('Classic? {}'.format(self.classic))
-        print('History: {}'.format(self.history))
-        print('Dimension: {}'.format(self.dim))
-        print('F: {}'.format(self.F))
-        print('P: {}'.format(self.P))
-        print('X: {}'.format(self.X))
-        print('H: {}'.format(self.H))
-        print('KG: {}'.format(self.KG))
-        print('Q: {}'.format(self.Q))
-        print('R: {}'.format(self.R))
-        print('Var: {}'.format(self.variance))
-        print('Covar: {}'.format(self.covariance))
-        print('**************************')
+        print('Classic? \t{}'.format(self.classic))
+        print('History: \t{}'.format(self.history))
+        print('Order:   \t{}'.format(self.dim))
+        self.print_2D_mat(self.F, 'F')
+        self.print_2D_mat(self.P, 'P')
+        self.print_2D_mat(self.KG, 'KG')
+        self.print_1D_mat(self.X, 'X')
+        self.print_1D_mat(self.H, 'H')
+        self.print_2D_mat(self.x_history, 'x-hist')
+        self.print_1D_mat(self.y_history, 'y-hist')
+        print('Trained: \t{}'.format(self.nTrained))
+        print('--------------------------')
 
-    def train_me(self, obs, model, init = True):
-        """
-        Master method to control the initial 
-        training of the filter.
-        """
-        myobs = np.array(obs)
-        mymodel = np.array(model)
-
-        if myobs.shape != mymodel.shape:
-            raise TypeError('Initial training set does not have conforming shapes.')
-        
-        if max(myobs.shape) != self.history:
-            raise TypeError('Initial history data are not compatible with length of history: {}'.format(self.history))
-
-
-        # Since it is the first time...
-        for ih in range(self.history):
-
-            print('Training #{}'.format(ih))
-
-            y = obs[ih] - model[ih]
-
-            for ij in range(self.dim):
-                self.H[ij, 0] = model[ih] ** ij
-
-            xf = self.X
-            print('\nxf = {}'.format(xf))
-
-            Pf = self.P + self.Q
-            print('\nPf = {}'.format(Pf))
-
-            self.KG = np.dot(Pf, self.H) / np.dot(np.dot(self.H.T, Pf), self.H)
-            print('\nKG = {}'.format(self.KG))
-
-            self.P = np.dot(np.eye(self.dim) - np.dot(self.KG, self.H.T), Pf)
-            print('\nPa = {}'.format(self.P))
-
-            self.X = xf + self.KG * (y - np.dot(self.H.T, xf))
-            print('\nXa = {}'.format(self.X))
-
-            self.x_history[:, ih] = self.X[:,0]
-            print('\nx-history = {}'.format(self.x_history))
-
-            self.y_history[ih, 0] = y - np.dot(self.H.T, self.X)
-            print('\ny-history = {}'.format(self.y_history))
-            print('-------------------------------------------- \n')
-
-
-
-        return
-
-
-
-
-
-    # def calculate_variance(self):
-    #     """
-    #     Method to calculate the variance of the bias.
-    #     """
-    #     return np.var(self.yV)
-
-    # def get_variance(self):
-    #     return self.variance
-
-    # def calculate_covariance(self):
-    #     return np.cov(self.xV)
-
-    # def calculate_obs_matrix(self, model):
-    #     for ij in range(self.dim):
-    #         self.H[ij] = model * (ij - 1)
-    #     return 
-
-    # def calculate_kalman_gain(self):
-    #     """
-    #     Method to calculate the Kalman gain.
-    #     """
-    #     P1 = self.P + self.covariance
-
-    #     Q = np.dot(np.dot(self.H, P1), self.H.T)
-
-    #     self.KG = np.dot(P1, self.H.T) / (Q + self.variance)
-
-    # def update_state_vector(self, obs, model):
-    #     dif = obs - model - np.dot(self.H, self.x)
-    #     self.x += self.KG * dif 
     
-    # def correct_measurement(self, old_model, measurement, hard_limit = None, onlyPos = True):
-    #     """
-    #     Make an attempt to correct the measurement or model output.
-    #     """
-    #     pre = 0.0
-    #     for ij in range(self.dim):
-    #         pre += self.x[ij] * old_model ** (ij - 1)
-
-    #     if hard_limit:
-    #         if pre > hard_limit:
-    #             pre = measurement
-    #         else:
-    #             pre += measurement
-    #     else:
-    #         pre += measurement
-
-    #     if onlyPos:
-    #         if pre < 0.0:
-    #             pre = 0.0
-        
-    #     return pre
-
-    # def update_matrices(self, obs, model):
-
-    #     myKG = np.zeros((self.KG.shape[0],1))
-    #     myH = np.zeros((self.H.shape[0],1))
-
-    #     myKG[:,0] = self.KG
-    #     myH[:,0] = self.H
-
-    #     D = np.eye(self.dim) - np.matmul(myKG, myH.T)
-
-    #     self.P = np.dot(D, self.P)
-
-    #     self.yV[0:self.history - 1] = self.yV[1:self.history]
-    #     self.yV[self.history - 1] = obs - model
-    #     for ij in range(self.dim):
-    #         self.yV[self.history - 1] -= self.x[ij] * model ** (ij - 1)
-
-    #     self.x_mat[:, 0:self.history - 1] = self.x_mat[:, 1:self.history]
-    #     self.x_mat[:, self.history - 1] = self.x[:]
-
-
-
-
-
 
 
 
--- Kalman/testScript.py
+++ Kalman/testScript.py
@@ -1,96 +1,52 @@
 import numpy as np
 from Kalman import Kalman
+from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
 
-    kf = Kalman(7, 2)
+    # Define some generic values
+    history = 7
+    order = 2
 
-    fcst = [9., 3., 6., 7., 11., 0.4, 4.2]
-    obs = [9.5, 2.4, 6.1, 7.7, 12.1, 1., 3.8]
-    model = obs + np.random.normal(0.0, 1.1, size = len(obs))
+    # Create some imaginary data
+    obs = np.random.normal(15.4, 2.3, size = 2000)
+    model = obs + np.random.normal(2.5, 0.5, size = len(obs))
 
-    kf.train_me(obs, model)
+    # Define the plot range and size of the figure
+    pltRange = range(1000, 1045)
+    figSize = (11,9)
+
+    # Start Testing
+    # Create an instance of the filter
+    kf = Kalman(history, order)
     
-    # for ij in range(1):
+    # Use the first 7 of them for training
+    obs_train = obs[:history]
+    model_train = model[:history]
 
-    #     # Inform
-    #     kf.dump_members(ij='Starting iterantion {:d}'.format(ij))
+    # The rest to be used dynamically
+    obs_dyn = obs[history:]
+    model_dyn = model[history:]
+    fcst = np.zeros_like(obs_dyn)
 
-    #     kf.train_me(obs, model)
+    # Perform an initial training of the model
+    kf.train_me(obs_train, model_train)
 
+    for ij in range(len(obs_dyn)):
+        # Provide a correction to the forecast
+        fcst[ij] = kf.adjust_forecast(model_dyn[ij])
 
-        # # Get the observations matrix based on old model prediction
-        # kf.calculate_obs_matrix(model[ij])
+        # Update filter
+        kf.train_me([obs_dyn[ij]], [model_dyn[ij]])
 
-        # # Calculate variance and covariance of the dataset
-        # kf.calculate_variance()
-        # kf.calculate_covariance()
-
-        # # Calculate Kalman gain
-        # kf.calculate_kalman_gain()
-
-        # # Update the state vector based on the difference between true value and model previous forecast 
-        # kf.update_state_vector(obs[ij], model[ij])
-
-        # # Get a new, corrected value of current forecast, considering the previous forecast value
-        # newVal = kf.correct_measurement(model[ij], fcst[ij], onlyPos=False)
-
-        # # Update working matrices with new input
-        # kf.update_matrices(obs[ij], model[ij])
-
-        # # Inform
-        # kf.dump_members(ij=ij)
-
-        # # Show the corrected value
-        # print('Corrected value: {}'.format(newVal))
-
-    # x_history = np.zeros((2,7))
-
-    # x_history[0,0] = 1.0
-    # x_history[1,0] = 2.5
-
-    # x_history[0,1] = 1.25
-    # x_history[1,1] = 2.68
-
-    # x_history[0,2] = 1.32
-    # x_history[1,2] = 2.89
-
-    # x_history[0,3] = 2.0
-    # x_history[1,3] = 2.8
-
-    # x_history[0,4] = 1.0
-    # x_history[1,4] = 2.9
-
-    # x_history[0,5] = 0.54
-    # x_history[1,5] = 3.1
-
-    # x_history[0,6] = 0.0
-    # x_history[1,6] = 0.2
-
-    # #print(x_history)
-
-    # S1 = np.zeros((2,1))
-
-    # for ij in range(6):
-    #     S1[0] += x_history[0,ij+1] - x_history[0,ij]
-    #     S1[1] += x_history[1,ij+1] - x_history[1,ij]
-
-    # S1 /= 7
-    # #print(S1)
-
-    # S2 = np.zeros((2,1))
-    # theSum = np.zeros((2,2))
-    # for ij in range(6):
-    #     for jj in range(2):
-    #         S2[jj] = x_history[jj, ij+1] - x_history[jj, ij] - S1[jj]
-
-    #     theSum += np.dot(S2, S2.T)
+    fig = plt.figure(figsize=figSize)
+    plt.plot(obs_dyn[pltRange], label='obs')
+    plt.plot(model_dyn[pltRange], label='model')
+    plt.plot(fcst[pltRange], label='kalman')
+    plt.legend()
+    plt.show()
 
 
-
-    # theSum /= 6
-    # print(theSum)
-    # print('')
-    # print(np.cov(x_history))
+    
 
     
